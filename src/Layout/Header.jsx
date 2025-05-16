0ba28b1194ec3c5a7a2a05dcb393bd058d1b2f62
remeessalam <remeesreme4u@gmail.com>
2025-05-16 15:13:01 +0530

    initial commit

---
+++ src/Layout/Header.jsx
@@ -0,0 +1,119 @@
+import { useState } from "react";
+import { Sun, Moon, Menu, X } from "lucide-react";
+import { useTheme } from "../Context/ThemeContext";
+import logo from "../assets/images/headerlogo.png";
+import { Link, useNavigate, useLocation } from "react-router-dom";
+
+const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const { theme, toggleTheme } = useTheme();
+  const isDarkMode = theme === "dark";
+  const location = useLocation();
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const navigate = useNavigate();
+  const navigateTo = () => {
+    navigate("/contact-us");
+  };
+
+  const isActive = (path) => location.pathname === path;
+
+  return (
+    <nav className="fixed w-full top-0 !z-50 bg-white dark:bg-darkblack shadow-md">
+      <div className="wrapper">
+        <div className="flex items-center justify-between py-3 md:py-3">
+          <div className="flex-shrink-0">
+            <div className=" w-auto text-black   rounded-xl">
+              <img src={logo} alt="logo" className="w-[5rem] md:w-[5.8rem]" />
+            </div>
+          </div>
+
+          <div className="hidden md:flex items-center space-x-8">
+            {["/", "/about-us", "/services", "/portfolio", "/blog"].map(
+              (path, index) => (
+                <Link
+                  key={index}
+                  to={path}
+                  className={` hover:text-black  dark:hover:text-white ${
+                    isActive(path)
+                      ? "text-primary font-bold dark:text-white"
+                      : "text-primary/80 dark:text-white/80"
+                  }`}
+                >
+                  {path === "/"
+                    ? "Home"
+                    : path.replace("/", "").replace("-", " ")}
+                </Link>
+              )
+            )}
+
+            <button
+              onClick={navigateTo}
+              className="primary-btn !bg-black !text-white dark:!text-white dark:!bg-primary"
+            >
+              Contact Us
+            </button>
+            <button
+              onClick={toggleTheme}
+              className={`p-2 rounded-full bg-gray-100 dark:bg-gray-800 dark:text-yellow-300 text-gray-600`}
+              aria-label="Toggle dark mode"
+            >
+              {isDarkMode ? <Sun size={20} /> : <Moon size={20} />}
+            </button>
+          </div>
+
+          <div className="md:hidden flex items-center space-x-4">
+            <button
+              onClick={toggleTheme}
+              className={`p-2 rounded-full bg-gray-100 dark:bg-gray-800 dark:text-yellow-300 text-gray-600`}
+              aria-label="Toggle dark mode"
+            >
+              {isDarkMode ? <Sun size={20} /> : <Moon size={20} />}
+            </button>
+            <button
+              onClick={toggleMenu}
+              className="text-gray-200 dark:text-white"
+              aria-label="Toggle menu"
+            >
+              {isMenuOpen ? <X size={24} /> : <Menu size={24} />}
+            </button>
+          </div>
+        </div>
+      </div>
+
+      {isMenuOpen && (
+        <div className="md:hidden">
+          <div className="px-2 pt-2 pb-3 space-y-1 bg-white dark:bg-gray-900">
+            {["/", "/about-us", "/services", "/portfolio", "/blog"].map(
+              (path, index) => (
+                <Link
+                  key={index}
+                  to={path}
+                  onClick={toggleMenu}
+                  className={`block px-3 py-2 rounded-md text-base font-medium text-gray-900 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-800 ${
+                    isActive(path) ? "bg-gray-100 dark:bg-gray-800" : ""
+                  }`}
+                >
+                  {path === "/"
+                    ? "Home"
+                    : path.replace("/", "").replace("-", " ")}
+                </Link>
+              )
+            )}
+            <button
+              onClick={navigateTo}
+              className="w-full text-center bg-primary  text-white px-6 py-2 rounded-md hover:bg-blue-600 transition-colors"
+            >
+              Contact Us
+            </button>
+          </div>
+        </div>
+      )}
+    </nav>
+  );
+};
+
+export default Header;
---
+++ src/util/services.js
@@ -0,0 +1,155 @@
+import webdevbannerimg from "../assets/images/services/appdevbannerimg.png";
+import appdevimg1 from "../assets/images/services/appdevimg1.png";
+import appdevimg2 from "../assets/images/services/appdevimg2.jpeg";
+import UX_UI_Design from "../assets/images/services/UX_UI_Design.png";
+import UX_UI_Sub1 from "../assets/images/services/UX_UI_Sub1.png";
+import UX_UI_Sub2 from "../assets/images/services/UX_UI_Sub2.png";
+import AR_VR_Development from "../assets/images/services/AR_VR_Development.jpeg";
+import AR_VR_sub1 from "../assets/images/services/AR_VR_sub1.png";
+import AR_VR_sub2 from "../assets/images/services/AR_VR_sub2.jpeg";
+import Full_Stack_Development from "../assets/images/services/Full_Stack_Development.png";
+import Full_Stack_sub2 from "../assets/images/services/Full_Stack_sub2.png";
+import Full_Stack_sub1 from "../assets/images/services/Full_Stack_sub1.jpeg";
+import AICallingAgency from "../assets/images/services/AICallingAgency.jpeg";
+import AICallingAgencysub1 from "../assets/images/services/AICallingAgencysub1.jpeg";
+import AICallingAgencysub2 from "../assets/images/services/AICallingAgencysub2.png";
+import Chatbot_Development from "../assets/images/services/Chatbot_Development.jpeg";
+import Chatbot_DevelopmentSub2 from "../assets/images/services/Chatbot_DevelopmentSub2.jpeg";
+import Chatbot_DevelopmentSub1 from "../assets/images/services/Chatbot_DevelopmentSub1.png";
+import Artificial_Intelligence_Solutions from "../assets/images/services/Artificial_Intelligence_Solutions.jpeg";
+import Artificial_Intelligence_Solutionssub1 from "../assets/images/services/Artificial_Intelligence_Solutionssub1.jpeg";
+import Artificial_Intelligence_Solutionssub2 from "../assets/images/services/Artificial_Intelligence_Solutionssub2.jpeg";
+import Data_Science_and_Analytics from "../assets/images/services/Data_Science_and_Analytics.jpeg";
+import Data_Science_and_Analyticssub1 from "../assets/images/services/Data_Science_and_Analyticssub1.jpeg";
+import Data_Science_and_Analyticssub2 from "../assets/images/services/Data_Science_and_Analyticssub2.png";
+import Robot_Process_automation from "../assets/images/services/Robot_Process_automation.png";
+import Robot_Process_automationSub1 from "../assets/images/services/Robot_Process_automationSub1.png";
+import Robot_Process_automationSub2 from "../assets/images/services/Robot_Process_automationSub2.jpeg";
+import Blockchain_Development from "../assets/images/services/Blockchain_DevelopmentSub1.png";
+import Blockchain_DevelopmentSub1 from "../assets/images/services/Robot_Process_automationSub1.png";
+import Blockchain_DevelopmentSub2 from "../assets/images/services/Blockchain_DevelopmentSub2.jpeg";
+import Game_Development from "../assets/images/services/Game_Development.png";
+import Game_DevelopmentSub1 from "../assets/images/services/Game_DevelopmentSub1.jpeg";
+import Game_DevelopmentSub2 from "../assets/images/services/Game_DevelopmentSub2.jpeg";
+import IoT_Development from "../assets/images/services/IoT_Development.jpeg";
+import IoT_DevelopmentSub1 from "../assets/images/services/IoT_DevelopmentSub1.jpeg";
+import IoT_DevelopmentSub2 from "../assets/images/services/IoT_DevelopmentSub2.jpeg";
+
+export const serviceDetails = [
+  {
+    id: 1,
+    bannerimg: Full_Stack_Development,
+    link: "app-development",
+    imageone: Full_Stack_sub1,
+    imgagetwo: Full_Stack_sub2,
+    title: "App Development",
+    heading: "At aicompany, Building Mobile Experiences That Matter",
+    description:
+      "We build custom iOS and Android apps that are smooth, scalable, and ready for real-world use. From MVPs to enterprise-grade systems, our mobile solutions are designed to perform — and keep your users coming back. Our development team focuses on creating applications that not only meet your business objectives but also provide exceptional user experiences. Whether you're launching a startup or enhancing an existing product, we deliver mobile solutions that stand out in today's competitive marketplace.",
+    secondHeading:
+      "How our company will Help you in the Era of App Development",
+    secondHeading_subheading: "Service Outcomes",
+    services: [
+      {
+        title: "Native & Cross-Platform Excellence",
+        description:
+          "Tailored solutions for both iOS and Android, ensuring maximum reach and performance.",
+      },
+      {
+        title: "User-Centered Design",
+        description:
+          "Every app is meticulously crafted with the end-user in mind, balancing aesthetics with functionality.",
+      },
+      {
+        title: "Enterprise-Grade Security",
+        description:
+          "We implement robust security measures to protect user data and ensure compliance with industry standards.",
+      },
+    ],
+    thirdHeading: "Redefine the Future with AI Innovation",
+    thirdDescription:
+      "At aicompany, we are revolutionizing mobile app development by integrating cutting-edge artificial intelligence capabilities. Our AI-powered apps deliver personalized experiences that adapt to user behavior, anticipate needs, and improve over time. From intelligent chatbots and voice assistants to advanced analytics and predictive features, we create mobile solutions that are not just functional but truly smart. By leveraging machine learning algorithms, we enable your app to process data more efficiently, make better decisions, and offer unparalleled value to your users. Partner with us to build future-ready applications that set new standards in the digital landscape.",
+    whyMatter:
+      "Why It Matters Globally: In today's hyper-connected world, mobile apps are essential touchpoints for customers across all demographics and regions. Our forward-thinking approach ensures your app resonates with diverse audiences while driving meaningful engagement and business growth.",
+    fourthHeading: "Accelerating Innovation Through App Development",
+    fourthDescripiton:
+      "At aicompany, we accelerate innovation through our comprehensive mobile development services by integrating advanced technologies such as artificial intelligence and machine learning. We transform ideas into market-ready applications with rapid development cycles, agile methodologies, and continuous iteration based on real user feedback. Our scalable architecture ensures your app can grow alongside your business, while our emphasis on performance optimization delivers lightning-fast experiences even under heavy load conditions. From seamless API integrations to robust backend systems, we build the complete ecosystem your mobile app needs to succeed in today's fast-paced digital marketplace.",
+  },
+  {
+    id: 2,
+    bannerimg: webdevbannerimg,
+    link: "web-development",
+    imageone: appdevimg1,
+    imgagetwo: appdevimg2,
+    title: "Web Development",
+    heading: "At aicompany, Creating Websites That Work — And Convert",
+    description:
+      "Your website isn't just a digital brochure — it's your engine for growth. We create fast, responsive, and SEO-optimized websites that make every click count. Our web development team combines technical expertise with strategic thinking to build online solutions that drive real business results. From corporate websites to complex web applications, we deliver digital experiences that engage visitors and convert them into loyal customers.",
+    secondHeading:
+      "How our company will Help you in the Era of Web Development",
+    secondHeading_subheading: "Service Outcomes",
+    services: [
+      {
+        title: "Responsive & Performance-Focused",
+        description:
+          "Websites that load quickly and display perfectly across all devices, maximizing user engagement and SEO ranking.",
+      },
+      {
+        title: "Conversion-Optimized Design",
+        description:
+          "Strategic layouts and user flows designed to guide visitors toward meaningful actions and business goals.",
+      },
+      {
+        title: "Scalable Architecture",
+        description:
+          "Future-proof solutions built on clean, maintainable code that can evolve with your business needs.",
+      },
+    ],
+    thirdHeading: "Redefine the Future with AI Innovation",
+    thirdDescription:
+      "Web development is undergoing a transformation through AI integration, enabling more intelligent, adaptive, and personalized digital experiences. At aicompany, we harness the power of artificial intelligence to create websites that learn from user interactions, optimize content delivery, and automate complex processes. From AI-powered chatbots and recommendation engines to intelligent content management systems and predictive analytics, our web solutions leverage cutting-edge technology to deliver exceptional results. By embracing these innovations, we help businesses stay ahead of digital trends and provide their users with smarter, more intuitive online experiences.",
+    whyMatter:
+      "Why It Matters for Digital Growth: Your web presence is often the first impression potential customers have of your business. Our development approach ensures that impression is not just positive but compelling enough to drive meaningful engagement and conversions across your digital ecosystem.",
+    fourthHeading: "Accelerating Innovation Through Web Development",
+    fourthDescripiton:
+      "Web development serves as a critical catalyst for business innovation by enabling companies to rapidly deploy, test, and refine digital solutions. At aicompany, we leverage modern development frameworks, microservices architecture, and continuous integration practices to accelerate the delivery of web projects. Our approach emphasizes flexibility and adaptability, allowing businesses to respond quickly to market changes and user feedback. By building scalable, component-based systems, we create web solutions that can evolve incrementally without requiring complete overhauls. This methodology not only speeds time-to-market but also fosters a culture of continuous improvement and digital transformation within organizations.",
+  },
+  {
+    id: 3,
+    bannerimg: UX_UI_Design,
+    link: "ui-ux-design",
+    imageone: UX_UI_Sub1,
+    imgagetwo: UX_UI_Sub2,
+    title: "UI/UX Design",
+    heading: "At aicompany, Design That Connects People to Product",
+    description:
+      "Design is more than visuals — it's about experience. Our UI/UX process blends strategy and creativity to build intuitive, user-centered interfaces that turn visitors into loyal users. Great products don't just look good — they feel right. From wireframes to final polish, we turn interfaces into experiences that resonate with your target audience and support your business objectives. Our human-centered design approach ensures that every element serves a purpose in the user journey.",
+    secondHeading: "How our company will Help you in the Era of UI/UX Design",
+    secondHeading_subheading: "Service Outcomes",
+    services: [
+      {
+        title: "User-Centered Research & Strategy",
+        description:
+          "Data-driven insights into user behaviors and preferences that inform all design decisions.",
+      },
+      {
+        title: "Intuitive Information Architecture",
+        description:
+          "Logical organization and flow that makes complex information accessible and navigation effortless.",
+      },
+      {
+        title: "Visual Design Excellence",
+        description:
+          "Attractive, consistent, and purposeful interfaces that reinforce your brand identity while enhancing usability.",
+      },
+    ],
+    thirdHeading: "Redefine the Future with AI Innovation",
+    thirdDescription:
+      "AI is revolutionizing UI/UX design by enabling more personalized, adaptive, and intelligent user experiences. At aicompany, we integrate artificial intelligence into our design process to analyze user behavior patterns, predict user preferences, and create interfaces that respond dynamically to individual needs. Our AI-enhanced design approach helps identify usability issues before they impact users, generates design variations based on performance metrics, and enables real-time personalization of content and layouts. By embracing these technological advancements, we create digital experiences that not only meet current user expectations but anticipate future needs, setting new standards in human-computer interaction.",
+    whyMatter:
+      "Why It Matters for User Engagement: In today's competitive digital landscape, the quality of user experience often determines success or failure. Our design philosophy centers on creating emotional connections between users and digital products, fostering loyalty, reducing friction, and ultimately driving sustainable business growth through superior user satisfaction.",
+    fourthHeading: "Accelerating Innovation Through UI/UX Design",
+    fourthDescripiton:
+      "UI/UX design serves as a powerful driver of innovation by bridging user needs with technological possibilities. At aicompany, our design process focuses on rapid ideation, prototyping, and validation, allowing businesses to explore new concepts and refine them based on real user feedback. By creating interactive prototypes early in the development cycle, we enable stakeholders to experience and evaluate potential solutions before significant resources are invested in development. This approach not only accelerates the innovation process but also reduces risk by ensuring that the final product addresses genuine user needs and preferences. Through strategic design thinking, we help organizations transform challenges into opportunities and deliver breakthrough digital experiences.",
+  },
+];
